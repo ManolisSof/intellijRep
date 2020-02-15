b7a72105e081ddddbb6f45eea57a25c5076072e3
ManolisSof <manos.sofikitis@gmail.com>
2020-02-15 15:07:44 +0200

    My first commit for this project

--- src/main/java/myPackage/FirstPass.java
+++ src/main/java/myPackage/FirstPass.java
@@ -1,6 +1,7 @@
 package myPackage;
 
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -15,12 +16,31 @@ public class FirstPass {
         env.getConfig().setGlobalJobParameters(params);
 
         // Read the csv file and put the second field and the seventh field in a tuple
-//        DataSet<Tuple2<String, Float>> csvInput = env.readCsvFile(params.get("input")).includeFields(0100001000)
-        DataSet<Tuple2<String, Float>> csvInput = env.readCsvFile("openaq_kabul.csv").includeFields(010000100000)
-                .types(String.class, Float.class);
+        // Read the file and create a tuple with 3 attributes: City_Name, Gas_Name, Measurement
+//        DataSet<Tuple3<String, String, Float>> csvInput = env.readTextFile("openaq_kabul.csv")
+//                .map(new MapFunction<String, Tuple3<String, String, Float>>() {
+//            @Override
+//            public Tuple3<String, String, Float> map(String value) throws Exception {
+//                String[] words = value.split(",");
+//                Tuple3<String, String, Float> t1 = new Tuple3<>(words[1], words[5], Float.parseFloat(words[6]));
+//                return t1;
+//            }
+//        });
 
+        // Read the file and create a tuple with 2 attributes: City_Name, Measurement
+        DataSet<Tuple2<String, Float>> csvInput = env.readTextFile("openaq_kabul.csv")
+                .map(new MapFunction<String, Tuple2<String, Float>>() {
+                    @Override
+                    public Tuple2<String, Float> map(String value) throws Exception {
+                        String[] words = value.split(",");
+                        Tuple2<String, Float> t1 = new Tuple2<>(words[1], Float.parseFloat(words[6]));
+                        return t1;
+                    }
+                });
 
-        csvInput.print();
+        DataSet<Tuple2<String, Float>> sum = csvInput.groupBy(new int[] {0}).sum(1);
+
+        sum.print();
 
         env.execute("OpenAq Job 1");
 
